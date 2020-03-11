cdbb4f0cbda12d25e299dd1186156baa261da5c8
aSpired92 <maciek120920@gmail.com>
2020-03-12 00:46:23 +0100

    1

--- src/main/java/pluginy/plugindrop/PluginDrop.java
+++ src/main/java/pluginy/plugindrop/PluginDrop.java
@@ -11,21 +11,48 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.block.Block;
 import org.bukkit.Bukkit;
 
+
 import java.util.ArrayList;
+import java.util.Random;
+
+import static org.apache.commons.lang.ArrayUtils.contains;
+import static org.bukkit.Material.*;
 
 public final class PluginDrop extends JavaPlugin implements Listener {
 
     ArrayList<ItemStack> dropsy = new ArrayList<ItemStack>();
-
+    Material[] droppingBlocks = {
+            STONE,
+            IRON_ORE,
+            COAL_ORE,
+            DIAMOND_ORE,
+            EMERALD_ORE,
+            GOLD_ORE,
+            LAPIS_ORE,
+            ANDESITE,
+            DIORITE,
+            GRANITE
+    };
     @Override
     public void onEnable()
     {
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
-        dropsy.add(new ItemStack(Material.SPONGE,1));
-        dropsy.add(new ItemStack(Material.GOLD_INGOT,1));
+        dropsy.add(new ItemStack(DIAMOND,1));
+        dropsy.add(new ItemStack(GOLD_INGOT,1));
+        dropsy.add(new ItemStack(IRON_INGOT,1));
+        dropsy.add(new ItemStack(EMERALD,1));
+        dropsy.add(new ItemStack(COAL,1));
+        dropsy.add(new ItemStack(REDSTONE,1));
+        dropsy.add(new ItemStack(LAPIS_LAZULI,1));
+        dropsy.add(new ItemStack(OBSIDIAN,1));
+        dropsy.add(new ItemStack(ENDER_PEARL,1));
+        dropsy.add(new ItemStack(TNT,1));
 
 
-        System.out.println("TEST: Jebłeś kostke?");
+
+
+
+        System.out.println("Uruchomienie NovumDrop przebieglo pomyslnie");
     }
 
     @Override
@@ -36,15 +63,14 @@ public final class PluginDrop extends JavaPlugin implements Listener {
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event)
     {
-
-        ItemStack Stak = dropsy.get(0);
+        Random rand = new Random();
+        Player gracz = event.getPlayer();
         Block cube = event.getBlock();
         event.setDropItems(false);
-        if(cube.getType() == Material.STONE)
+        if(contains(droppingBlocks,cube.getType()))
         {
-
-            System.out.println("Jebłeś kostke");
-            cube.getWorld().dropItemNaturally(cube.getLocation(), Stak);
+            gracz.getInventory().addItem(dropsy.get(rand.nextInt(dropsy.size())));
+            //cube.getWorld().dropItemNaturally(cube.getLocation(), dropsy.get(0));
         }
     }
 }
